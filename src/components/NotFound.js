003bfade1906942cf110a9b4f73b0e7e3c60b009
valensartorelli <valen.sartorelli@gmail.com>
2021-08-06 23:49:41 -0300

    Valen - Acomodo NotFound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import imagenFondo from '../assets/images/404.png';
+import TopBar from "./TopBar";
+import Footer from "./Footer";
 
 function NotFound(){
     return(
-        <div className="text-center">
-            <h1>404 Not Found</h1>
-            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 30 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-        </div>
+        <React.Fragment>
+            <div id="content-wrapper" class="d-flex flex-column">
+                <div id="content">
+                    <TopBar />
+                    <div className="text-center">
+                        <div className="cont-totales">
+                            <h1>404 Not Found</h1>
+                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 30 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            </div>
+        </React.Fragment>
+
+        
         
     )
 }
